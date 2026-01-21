eb46942bb47c79936f7253e028945f555554f060
George Al Akhras <george.alakhras@hotmail.com>
2026-01-21 17:56:05 +0100

    1.0 release

--- dbLabb/Models/Link.cs
+++ dbLabb/Models/Link.cs
@@ -1,10 +1,12 @@
-﻿namespace dbLabb.Models
+﻿using System.Text.Json.Serialization;
+namespace dbLabb.Models
 {
     public class Link
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public int IntrestId { get; set; }
+        [JsonIgnore]
         public Interest Interest { get; set; }  
     }
 }
--- dbLabb/Program.cs
+++ dbLabb/Program.cs
@@ -1,6 +1,7 @@
-
 using dbLabb.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using dbLabb.Models;
 
 namespace dbLabb
 {
@@ -27,15 +28,162 @@ namespace dbLabb
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-            app.MapGet("/getall", async (InterestingDbContext context) =>
+            app.MapGet("/getallpersons", async (InterestingDbContext context) =>
             {
+                try
+                {
+                    var fetchedUsers = await context.Persons.ToListAsync();
+                    if (fetchedUsers == null || fetchedUsers.Count == 0)
+                    {
+                        return Results.NoContent();
+                    }
+                    else
+                    {
+                        return Results.Ok(fetchedUsers);
+                    }
+                }
+                catch
+                {
+                    return Results.InternalServerError();
+                }
+            });
 
+            app.MapGet("/getspecific/Interests{id}", async (InterestingDbContext context, int id) =>
+            {
+                try
+                {
+                    var fetchedIntrests = await context.Interests
+                    .Where(i => i.PersonId == id)
+                    .ToListAsync();
+                    if (fetchedIntrests == null || fetchedIntrests.Count == 0)
+                    {
+                        return Results.NoContent();
+                    }
+                    else
+                    {
+                        return Results.Ok(fetchedIntrests);
+                    }
+                }
+                catch
+                {
+                    return Results.InternalServerError();
+                }
+            });
+
+            app.MapGet("/getspecific/Links{id}", async (InterestingDbContext context, int id) =>
+            {
+                try
+                {
+                    var fetchedLinks = await context.Links
+                    .Where(i => i.Interest.PersonId == id)
+                    .ToListAsync();
+
+                    if (fetchedLinks == null || fetchedLinks.Count == 0)
+                    {
+                        return Results.NoContent();
+                    }
+                    else
+                    {
+                        return Results.Ok(fetchedLinks);
+                    }
+                }
+                catch
+                {
+                    return Results.InternalServerError();
+                }
+            });
+
+            app.MapPost("/addtoperson/interests", async (InterestingDbContext context, Interest newInterest) =>
+            {
+                try
+                {
+                    var foundPerson = await context.Persons
+                    .FindAsync(newInterest.PersonId);
+
+                    var transaction = await context.Database.BeginTransactionAsync();
+
+
+                    if (foundPerson == null)
+                    {
+                        transaction.Rollback();
+                        return Results.NotFound();
+                    }
+                    else
+                    {
+                        var interest = new Interest
+                        {
+                            PersonId = newInterest.PersonId,
+                            Name = newInterest.Name
+                        };
+                        await context.Interests.AddAsync(interest);
+
+                        await transaction.CommitAsync();
+                        await context.SaveChangesAsync();
+
+                        return Results.Ok(newInterest);
+                    }
+                }
+                catch
+                {
+                    return Results.InternalServerError();
+                }
+            });
+
+            app.MapPost("/addlink/person={personId}&interest={interestId}", async (InterestingDbContext context, Link link, int personId, int interestId) =>
+            {
+                try
+                {
+                    var transaction = await context.Database.BeginTransactionAsync();
+                    var fetchedPerson = await context.Persons
+                        .Where(p => p.Id == personId)
+                        .Include(p => p.Interests)
+                        .ToListAsync();
+
+                    if (fetchedPerson != null)
+                    {
+                        var foundInterest = new Interest();
+
+                        foreach (var person in fetchedPerson)
+                        {
+                            var interestList = person.Interests;
+                            foreach (var i in interestList)
+                            {
+                                if (i.Id == interestId)
+                                {
+                                    foundInterest = i;
+                                }
+                            }
+                        }
+
+                        var newLink = new Link
+                        {
+                            Url = link.Url,
+                            IntrestId = foundInterest.Id
+                        };
+                        await context.Links.AddAsync(newLink);
+                        await transaction.CommitAsync();
+                        await context.SaveChangesAsync();
+
+                        return Results.Ok(newLink);
+                    }
+                    else
+                    {
+                        transaction.Rollback();
+                        return Results.NotFound();
+                    }
+
+                }
+                catch
+                {
+                    return Results.InternalServerError();
+                }
             });
 
             app.Run();
